6d6066d9e13a27116d7c8687c08a2c181c765f77
lucaszebre <yakirisk@gmail.com>
2023-08-11 17:06:37 +0200

    post,get

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const PORT = 3000
 const app= express()
+
+app.use(express.json());
+app.use(express.urlencoded());
 app.listen(PORT,()=>{
     console.log(`Running express server on Port ${PORT}`)
+})
+
+app.get('/groceries',(req,res,next)=>{
+    console.log('middleware')
+    next()
+},(req,res)=>{
+    res.send([{
+        item:'milk',
+        chocolat:'3'
+    },{
+        item:'peanut',
+        eating:'yes'
+    }])
+}
+)
+
+app.post('/groceries',(req,res)=>{
+    console.log(req.body);
+    res.send(201);
 })
